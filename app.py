331841934d49e13595f8381ea030a5fa1ffdbdd0
Suwung <suwungtandon@gmail.com>
2019-04-14 11:40:19 +0700

    edit
    Analis

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, render_template, jsonify
 
 app = Flask(__name__)
 # Setup the client <query string, retweets_only bool, with_sentiment bool>
-api = TwitterClient('@shivangbansal')
+api = TwitterClient('@FTaufich')
 
 
 def strtobool(v):
